ef011a3fa3b092a93c60bcc529ddaa34501c86a5
ellisPae <ellis.h.pae@gmail.com>
2020-09-21 18:09:44 -0400

    fix initial push

--- db/migrate/20200920155452_create_users.rb
+++ db/migrate/20200920155452_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.integer :id, null: false
       t.string :email, null: false
-      t.string :username, null: false,
+      t.string :username, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
       t.integer :genre_id, null: false 
